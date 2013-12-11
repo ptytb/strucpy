071210ac511d2451b0d2c766263a9fa6245f06b0
proninis <luminex@bk.ru>
2013-12-11 19:16:01 +0400

    Now generating tidy code, add wrapper, cleanups

--- strucpy.py
+++ strucpy.py
@@ -4,11 +4,13 @@ from pycparserext.ext_c_parser import GnuCParser
 from pycparser import parse_file
 from pycparser.c_parser import ParseError
 from pycparser.c_ast import *
+from textwrap import *
 
 ast = None
-hookDict = {}
+hookDict = []
 output = None
 header = None
+gcp = GnuCParser()
 
 def gettype(NAME):
         class TypedeclVisitor(NodeVisitor):
@@ -25,7 +27,8 @@ def gettype(NAME):
 
 def write_c(s):
     global output
-    output.write(s + "\n")
+    output.write("\n".join(wrap(s, break_long_words=False)))
+    output.write("\n")
 
 def write_h(s):
     global header
@@ -41,10 +44,12 @@ def askArrayLengthField():
 
 def isStdType(NAME):
     return NAME in ("int", "float", "void", "char", "signed",
-                    "unsigned", "auto", "short", "long")
+                    "unsigned", "auto", "short", "long", "wchar_t")
 
 def isPodType(TYPE):
-#    print "checking {0}\n".format(TYPE)
+    """ Check type for being plain-old-data type, which can be
+    just copied with memcpy"""
+
     if hasattr(TYPE, "POD"):
         return TYPE.POD
 
@@ -52,45 +57,68 @@ def isPodType(TYPE):
     result = False
 
     if t in (Typedef, TypeDecl, Decl, ArrayDecl):
-#        if t is Typedef:
-#            print "checking type {0}\n".format(TYPE.name)
-        result = all(map(isPodType, (TYPE.type,)))
-    elif t in (Struct, Union):
+        result = isPodType(TYPE.type)
+    elif t is Struct:
         result = all(map(isPodType, fields(TYPE)))
     elif t is IdentifierType:
         result = all(map(
-            lambda n : True if isStdType(n) else isPodType(gettype(n))
+            lambda n : isStdType(n) or isPodType(gettype(n))
                 , TYPE.names));
-    elif t is (Constant,):
+    elif t in (Constant, Union):
         result = True
 
     TYPE.POD = result
     return result;
 
 def isPointer(TYPE):
-    return type(TYPE) is PtrDecl \
-                or hasattr(TYPE, "type") \
-                and type(TYPE.type) is PtrDecl;
+    return type(TYPE) is PtrDecl 
+#               or hasattr(TYPE, "type") \
+#               and type(TYPE.type) is PtrDecl;
 
 def deref(TYPE):
+    """ Return subnode if pointer, grandchild if sub is pointer """
     if type(TYPE) is PtrDecl:
         return TYPE.type
-    elif hasattr(TYPE, "type") and type(TYPE.type) is PtrDecl:
-        return TYPE.type.type
+#    elif hasattr(TYPE, "type") and type(TYPE.type) is PtrDecl:
+#        return TYPE.type.type
     return None
 
 def fields(TYPE):
-    return TYPE.decls if type(TYPE) in [ Struct, Union ] else []
+    return TYPE.decls if type(TYPE) is Struct else []
 
-def hooked(ATYPE, BTYPE):
-    pass
+def sethook(ATYPE, BTYPE, FUNC):
+    hookDict.append((ctype(ATYPE), ctype(BTYPE), FUNC, ));
 
-def hook(T, A, B):
-    pass
+def gethook(ATYPE, BTYPE, NAME):
+    for h in hookDict:
+        if isCtypeEq(ATYPE, h[0]) and isCtypeEq(BTYPE, h[1]):
+            return "%s(%s)" % (h[2], NAME)
+    return None
+
+def ctype(TYPESTR):
+    global gcp
+    a = gettype(TYPESTR)
+    if not a:
+        a = gcp.parse(TYPESTR + " X;").ext[0].type
+    return a
+
+def isCtypeEq(ATYPE, BTYPE):
+    t = type(ATYPE)
+    if t is type(BTYPE):
+        if t in (Typedef, TypeDecl, Decl, ArrayDecl, PtrDecl):
+            return isCtypeEq(ATYPE.type, BTYPE.type)
+        elif t is Struct:
+            return all(map(isCtypeEq, fields(ATYPE), fields(BTYPE) ))
+        elif t is IdentifierType:
+            return ATYPE.names == BTYPE.names;
+        elif t is (Constant,):
+            return ATYPE.value == BTYPE.value
+    return False 
 
 def struct_copy_rec(A, ATYPE, B, BTYPE):
-    if hooked(ATYPE, BTYPE):
-        write_c(hook((ATYPE, BTYPE,), A, B))
+    h = gethook(ATYPE, BTYPE, A)
+    if h:
+        write_c("%s = %s;" % (B, h))
         return
 
     while type(ATYPE) in (Typedef, TypeDecl):
@@ -110,23 +138,32 @@ def struct_copy_rec(A, ATYPE, B, BTYPE):
             ATYPE = ATYPE.type
         write_c("memcpy({0}, {1}, sizeof({2}));"
                 .format(A, B, size))
-        return
-    
+
     elif isPodType(ATYPE):
         write_c("{0} = {1};".format(B, A));
-        return
-    elif isPointer(ATYPE):
-        write_c("if (({0} = {1}) != NULL) {{".format(B, A))
+    elif isPointer(ATYPE) and type(deref(ATYPE)) is TypeDecl and \
+        type(deref(ATYPE).type) is Struct and not isPodType(deref(ATYPE).type):
+#pointer to non-POD struct, use "->"
+        write_c("if (({0} = {1}) != NULL)\n{{".format(B, A))
         write_c("{0} = malloc(sizeof(*{0}));".format(B))
+        ATYPE = deref(ATYPE).type
+        for F in fields(ATYPE):
+            struct_copy_rec("{0}->{1}".format(A, F.name), F.type, \
+                            "{0}->{1}".format(B, F.name), F.type)
+        write_c("}")
+    elif isPointer(ATYPE):
+        write_c("if (({0} = {1}) != NULL)\n{{".format(B, A))
+        write_c("{0} = malloc(sizeof(*{0}));".format(B))
+
         struct_copy_rec("(*{0})".format(A), deref(ATYPE),
                         "(*{0})".format(B), deref(BTYPE))
         write_c("}")
-
-    if type(ATYPE) in [ Struct, Union ]:
+    elif type(ATYPE) is Struct:
         for F in fields(ATYPE):
-            struct_copy_rec("({0}).{1}".format(A, F.name), F.type, \
-                            "({0}).{1}".format(B, F.name), F.type)
+            struct_copy_rec("{0}.{1}".format(A, F.name), F.type, \
+                            "{0}.{1}".format(B, F.name), F.type)
 
+#TODO: fix array support
 ##if askIsArray(A):
 ##            L = askArrayLengthField()
 ##            if isPodType(deref(ATYPE)):
@@ -160,10 +197,15 @@ def processFiles(FILES):
 
 	for f in FILES:
 		try:
-			gcp = GnuCParser()
 			ast = parse_file(f, parser=gcp)
 		except ParseError as e:
 			print "Parse error: " + e.message
+                        break
+
+                sethook("char *", "char *", "strdup")
+
+                #TODO: hook any types loaded in AST
+                #sethook("LPSTR", "LPSTR", "strdup")
 
 		write_h("#include \"" + f + "\"")
 		struct_copy("LPWSAQUERYSETA", "LPWSAQUERYSETA");
@@ -172,8 +214,12 @@ def processFiles(FILES):
 	output.close()
 	header.close()
 
-
 processFiles(("example.h",))
-#processFiles(("/media/usb3/media/Src/wine/wine/include/ws2def.h",))
 
+#a = ctype("const int *")
+#b = ctype("int *")
+#print a == b
+#print isCtypeEq(a, b)
+
+#processFiles(("/media/usb3/media/Src/wine/wine/include/ws2def.h",))
 
